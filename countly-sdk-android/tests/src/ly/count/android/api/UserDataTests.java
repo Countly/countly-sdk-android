d14f64bd484459d5c1638ec24d4cf57a8d37aad1
Artem Salpagarov <artem.salpagarov@gmail.com>
2014-11-07 18:35:24 +0300

    Fixing tests for UserData-related functionality
    Conflicts:
    	countly-sdk-android/AndroidManifest.xml
    	countly-sdk-android/tests/AndroidManifest.xml

--- countly-sdk-android/tests/src/ly/count/android/api/UserDataTests.java
+++ countly-sdk-android/tests/src/ly/count/android/api/UserDataTests.java
@@ -1,9 +1,13 @@
-import ly.count.android.api.UserData;
+package ly.count.android.api;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.test.AndroidTestCase;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class UserDataTests extends AndroidTestCase {
@@ -17,7 +21,6 @@ public class UserDataTests extends AndroidTestCase {
 		data.putString("gender", "M");
 		data.putString("picture", "http://domain.com/test.png");
 		data.putInt("byear", 2000);
-		
         UserData.setData(data);
         
         assertEquals("Test Test", UserData.name);
@@ -30,7 +33,18 @@ public class UserDataTests extends AndroidTestCase {
         assertEquals(2000, UserData.byear);
 	}
 	
-	public void testJSON(){
+	public void testJSON() throws JSONException{
+        Bundle data = new Bundle();
+        data.putString("name", "Test Test");
+        data.putString("username", "test");
+        data.putString("email", "test@gmail.com");
+        data.putString("organization", "Tester");
+        data.putString("phone", "+1234567890");
+        data.putString("gender", "M");
+        data.putString("picture", "http://domain.com/test.png");
+        data.putInt("byear", 2000);
+        UserData.setData(data);
+
 		JSONObject json = UserData.toJSON();
 		assertEquals("Test Test", json.getString("name"));
         assertEquals("test", json.getString("username"));
@@ -39,10 +53,10 @@ public class UserDataTests extends AndroidTestCase {
         assertEquals("+1234567890", json.getString("phone"));
         assertEquals("M", json.getString("gender"));
         assertEquals("http://domain.com/test.png", json.getString("picture"));
-        assertEquals(2000, json.getString("byear"));
+        assertEquals(2000, json.getInt("byear"));
 	}
 	
-	public void testPicturePath(){
+	public void testPicturePath() throws MalformedURLException{
 		String path = "http://test.com/?key1=val1&picturePath=%2Fmnt%2Fsdcard%2Fpic.jpg&key2=val2";
 		String picturePath = UserData.getPicturePathFromQuery(new URL(path));
 		assertEquals("/mnt/sdcard/pic.jpg", picturePath);
