5991a7d0eff01ec98c243d98f671d63573211c47
Arturs Sosins <ar2rsawseen@gmail.com>
2015-12-06 14:01:19 +0200

    [views] need to pass activity on start to determine view name

--- app/src/main/java/ly/count/android/demo/MainActivity.java
+++ app/src/main/java/ly/count/android/demo/MainActivity.java
@@ -164,7 +164,7 @@ public class MainActivity extends Activity {
     public void onStart()
     {
         super.onStart();
-        Countly.sharedInstance().onStart();
+        Countly.sharedInstance().onStart(this);
     }
 
     @Override
