4da13547fd36d8e6f98da00f1d09e7a82b01f4fc
Artem Salpagarov <a@ahoy.li>
2016-08-19 17:56:12 +0300

    Allowing ECDHE_RSA

--- sdk/src/main/java/ly/count/android/sdk/CertificateTrustManager.java
+++ sdk/src/main/java/ly/count/android/sdk/CertificateTrustManager.java
@@ -51,7 +51,7 @@ public final class CertificateTrustManager implements X509TrustManager {
             throw new IllegalArgumentException("PublicKeyManager: X509Certificate is empty");
         }
 
-        if (!(null != authType && authType.equalsIgnoreCase("RSA"))) {
+        if (!(null != authType && authType.contains("RSA"))) {
             throw new CertificateException("PublicKeyManager: AuthType is not RSA");
         }
 
