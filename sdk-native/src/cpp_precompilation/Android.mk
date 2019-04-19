6a575da22386ec38d9c43fff8cef552a85458f83
pembeci <pembeci@gmail.com>
2019-04-19 10:13:32 +0300

    Use LOCAL_MODULE_FILENAME to prevent NDK full rebuild everytime.

--- sdk-native/src/cpp_precompilation/Android.mk
+++ sdk-native/src/cpp_precompilation/Android.mk
@@ -4,7 +4,8 @@ include $(ROOT_PATH)/breakpad/android/google_breakpad/Android.mk
 LOCAL_PATH := $(ROOT_PATH)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := countly_native
+LOCAL_MODULE    := breakpad
+LOCAL_MODULE_FILENAME := countly_native
 LOCAL_SRC_FILES := countly_breakpad.cpp
 LOCAL_LDLIBS := -llog
 LOCAL_STATIC_LIBRARIES += breakpad_client
