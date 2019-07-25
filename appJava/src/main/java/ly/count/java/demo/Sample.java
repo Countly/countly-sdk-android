168377740a3a0cb5a29b06710107137d49014c69
ArtursK <kadikis.arturs@gmail.com>
2019-07-25 21:30:18 +0300

    Adding java sample app module

---
+++ appJava/src/main/java/ly/count/java/demo/Sample.java
@@ -0,0 +1,10 @@
+package ly.count.java.demo;
+
+import ly.count.sdk.java.Countly;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Whaaaat " + 2341);
+        //Countly.api()
+    }
+}
