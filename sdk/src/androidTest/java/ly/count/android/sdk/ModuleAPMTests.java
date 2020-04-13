6f0ca47279c3cc88e1c0e0017c0bacea7790a95c
ArtursK <kadikis.arturs@gmail.com>
2020-04-13 17:27:29 +0300

    Adding test file

---
+++ sdk/src/androidTest/java/ly/count/android/sdk/ModuleAPMTests.java
@@ -0,0 +1,27 @@
+package ly.count.android.sdk;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static androidx.test.InstrumentationRegistry.getContext;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.booleanThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@RunWith(AndroidJUnit4.class)
+public class ModuleAPMTests {
+}
