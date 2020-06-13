0dad565b6ffc82456f2e3256cbd692c07cd5815d
ArtursK <kadikis.arturs@gmail.com>
2020-06-13 05:11:34 +0300

    Preperations for 20.04.3

--- sdk/src/main/java/ly/count/android/sdk/Countly.java
+++ sdk/src/main/java/ly/count/android/sdk/Countly.java
@@ -55,7 +55,7 @@ public class Countly {
     /**
      * Current version of the Count.ly Android SDK as a displayable string.
      */
-    public static final String COUNTLY_SDK_VERSION_STRING = "20.04.2";
+    public static final String COUNTLY_SDK_VERSION_STRING = "20.04.3";
     /**
      * Used as request meta data on every request
      */
