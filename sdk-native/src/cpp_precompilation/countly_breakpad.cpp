7e582e16e342209432e88b0ce419d37e15a32c2b
Ata Kuyumcu <ata.kuyumcu@hey.com>
2020-08-14 09:24:04 +0300

    Added breakpad version getter functions to the native sdk

--- sdk-native/src/cpp_precompilation/countly_breakpad.cpp
+++ sdk-native/src/cpp_precompilation/countly_breakpad.cpp
@@ -4,6 +4,7 @@
 
 #include <string.h>
 #include <jni.h>
+#include "breakpad/src/config.h"
 #include "breakpad/src/client/linux/handler/exception_handler.h"
 #include "breakpad/src/client/linux/handler/minidump_descriptor.h"
 #include <android/log.h>
@@ -27,13 +28,13 @@
 	#define LOGE(...) {}
 #endif
 
-// we need this to get Java env in DumpCallback. 
+// we need this to get Java env in DumpCallback.
 // See: https://stackoverflow.com/questions/12420463/keeping-a-global-reference-to-the-jnienv-environment
 
 static JavaVM *jvm;
 static jclass globalClass;
-static jmethodID mid;  
-bool callbackJava = false; 
+static jmethodID mid;
+bool callbackJava = false;
 
 bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
                   void* context,
@@ -41,27 +42,27 @@ bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
     LOGD("DumpCallback started");
     /*
     if (callbackJava) {
-	    JNIEnv *env; 
-	    jint rs = jvm->AttachCurrentThread(&env, NULL);    
+	    JNIEnv *env;
+	    jint rs = jvm->AttachCurrentThread(&env, NULL);
 	    assert (rs == JNI_OK);
-	    LOGD("DumpCallback: thread attached");    
+	    LOGD("DumpCallback: thread attached");
 	    env->CallStaticVoidMethod(globalClass, mid);
 	    jboolean flag = env->ExceptionCheck();
 	    if (flag) {
 	      env->ExceptionClear();
-	      LOGD("DumpCallback: exception occured in java callback."); 
+	      LOGD("DumpCallback: exception occured in java callback.");
 	    }
 	    else {
-	      LOGD("DumpCallback: java callback called successfully."); 
-	      jvm->DetachCurrentThread();  
-	    }   
+	      LOGD("DumpCallback: java callback called successfully.");
+	      jvm->DetachCurrentThread();
+	    }
     }
     */
-    LOGD("DumpCallback ==> succeeded %d path=%s", succeeded, descriptor.path());    
+    LOGD("DumpCallback ==> succeeded %d path=%s", succeeded, descriptor.path());
     return succeeded;
 }
 
-JavaVM* gJvm = nullptr; 
+JavaVM* gJvm = nullptr;
 
 extern "C" {
 
@@ -82,8 +83,8 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *pjvm, void *reserved) {
     mid = env->GetStaticMethodID(myClass, "processDump", "()V");
     LOGD("processDump found");
     env->CallStaticVoidMethod(myClass, mid);
-    LOGD("processDump called");    
-    globalClass = reinterpret_cast<jclass>(env->NewGlobalRef(myClass));    
+    LOGD("processDump called");
+    globalClass = reinterpret_cast<jclass>(env->NewGlobalRef(myClass));
     // mid = method2;
     return JNI_VERSION_1_6;
 }
@@ -95,23 +96,32 @@ JNIEXPORT jint JNICALL Java_ly_count_android_sdknative_CountlyNative_testCrash(J
     char *ptr = NULL; *ptr = 1;
     LOGE("native crash capture end. not expected to reach this line.");
     return 0;
-} 
+}
 
 JNIEXPORT jint JNICALL Java_ly_count_android_sdknative_CountlyNative_init(JNIEnv* env,
                                                                       jobject jobj,
-                                                                      jstring crash_dump_path){   
+                                                                      jstring crash_dump_path){
     // store JavaVM in global variable jvm
     // jint rs = env->GetJavaVM(&jvm);
     // assert (rs == JNI_OK);
 
     // init breakpad
-    const char* path = (char *)env->GetStringUTFChars(crash_dump_path, NULL);    
+    const char* path = (char *)env->GetStringUTFChars(crash_dump_path, NULL);
     google_breakpad::MinidumpDescriptor descriptor(path);
-    static google_breakpad::ExceptionHandler eh(descriptor, NULL, DumpCallback, NULL, true, -1);    
+    static google_breakpad::ExceptionHandler eh(descriptor, NULL, DumpCallback, NULL, true, -1);
     LOGD("breakpad initialized succeeded. dump files will be saved at %s", path);
     env->ReleaseStringUTFChars(crash_dump_path, path);
     return 1;
 }
 
+JNIEXPORT jstring JNICALL Java_ly_count_android_sdknative_CountlyNative_getBreakpadVersion(JNIEnv* env,
+                                                                      jobject jobj){
+    return env->NewStringUTF(VERSION);
+}
+
+JNIEXPORT jstring JNICALL Java_ly_count_android_sdknative_CountlyNative_getBreakpadChecksum(JNIEnv* env,
+                                                                      jobject jobj){
+    return env->NewStringUTF(CLY_HEAD_CHECKSUM);
+}
 
 }
