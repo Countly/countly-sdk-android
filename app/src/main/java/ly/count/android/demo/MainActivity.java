7749524924ea4b8bc4d14a588b52888c07081347
Zahid Zafar <>
2020-12-17 19:31:40 +0500

    Referrer Example Code move to Main Activity

--- app/src/main/java/ly/count/android/demo/MainActivity.java
+++ app/src/main/java/ly/count/android/demo/MainActivity.java
@@ -6,14 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.android.installreferrer.api.InstallReferrerClient;
+import com.android.installreferrer.api.InstallReferrerStateListener;
+import com.android.installreferrer.api.ReferrerDetails;
 import java.util.HashMap;
 
 import ly.count.android.sdk.Countly;
-
+import org.json.JSONObject;
 
 @SuppressWarnings("UnusedParameters")
 public class MainActivity extends AppCompatActivity {
     private String demoTag = "CountlyDemo";
+    InstallReferrerClient referrerClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Countly.onCreate(this);
+        referrerClient = InstallReferrerClient.newBuilder(this).build();
+        referrerClient.startConnection(new InstallReferrerStateListener() {
+            @Override
+            public void onInstallReferrerSetupFinished(int responseCode) {
+                switch (responseCode) {
+                    case InstallReferrerClient.InstallReferrerResponse.OK:
+                        try {
+                            ReferrerDetails response = referrerClient.getInstallReferrer();
+
+                            String referrerUrl = response.getInstallReferrer();
+
+                            HashMap<String, Object> custom = new HashMap<>();
+                            custom.put("install_referrer", referrerUrl);
+                            custom.put("referrer_click_timestamp_seconds", response.getReferrerClickTimestampSeconds());
+                            custom.put("install_begin_timestamp_seconds", response.getInstallBeginTimestampSeconds());
+                            custom.put("google_play_instant", response.getGooglePlayInstantParam());
+                            custom.put("install_version", response.getInstallVersion());
+
+                            JSONObject jsonObject = new JSONObject(custom);
+                            Countly.userData.setProperty("custom_referrer", jsonObject.toString(1));
+                            Countly.userData.save();
+
+                            referrerClient.endConnection();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
+                        // API not available on the current Play Store app.
+                        break;
+                    case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
+                        // Connection couldn't be established.
+                        break;
+                }
+            }
+
+            @Override
+            public void onInstallReferrerServiceDisconnected() {
+            }
+        });
     }
 
     public void onClickButtonCustomEvents(View v) {
@@ -61,10 +105,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickButtonRatings(View v) {
         startActivity(new Intent(this, ActivityExampleFeedback.class));
-    }
-
-    public void onClickButtonReferrer(View v) {
-        startActivity(new Intent(this, ActivityExampleReferrer.class));
     }
 
 
