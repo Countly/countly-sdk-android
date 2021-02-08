4c443e8498c7736f6dbeebbe6e23f31580db804a
Zahid Zafar <zahidzafar51@gmail.com>
2021-02-09 04:26:45 +0500

    [SDK-221] [Android] Creating referrer example  (#126)
    * Referrer Sample Added via new Library
    
    * Referrer Example Code move to Main Activity
    
    * Steps added to Test Referrer
    
    Co-authored-by: Zahid Zafar <>

--- app/src/main/java/ly/count/android/demo/MainActivity.java
+++ app/src/main/java/ly/count/android/demo/MainActivity.java
@@ -6,14 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.android.installreferrer.api.InstallReferrerClient;
+import com.android.installreferrer.api.InstallReferrerStateListener;
+import com.android.installreferrer.api.ReferrerDetails;
 import java.util.HashMap;
 
 import ly.count.android.sdk.Countly;
-
+import org.json.JSONObject;
 
 @SuppressWarnings("UnusedParameters")
 public class MainActivity extends AppCompatActivity {
     private String demoTag = "CountlyDemo";
+    InstallReferrerClient referrerClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Countly.onCreate(this);
+
+        /*
+        To send Referrer follow these steps.
+        Steps:
+        1. Start Google Play on the device using campaign link,
+        for example, https://play.google.com/store/apps/details?id=com.test.test_project&referrer=utm_source%3Dtest_source%26utm_medium%3Dtest_medium%26utm_term%3Dtest-term%26utm_content%3Dtest_content%26utm_campaign%3Dtest_name
+        (You can use google play generator: https://developers.google.com/analytics/devguides/collection/android/v3/campaigns#google-play-url-builder)
+        2. DON'T TAP ON INSTALL BUTTON
+        3. Install your test build using adb.
+
+        Google Play will be returning your test campaign now.
+         */
+        referrerClient = InstallReferrerClient.newBuilder(this).build();
+        referrerClient.startConnection(new InstallReferrerStateListener() {
+            @Override
+            public void onInstallReferrerSetupFinished(int responseCode) {
+                switch (responseCode) {
+                    case InstallReferrerClient.InstallReferrerResponse.OK:
+                        try {
+                            ReferrerDetails response = referrerClient.getInstallReferrer();
+
+                            String referrerUrl = response.getInstallReferrer();
+
+                            HashMap<String, Object> custom = new HashMap<>();
+                            custom.put("install_referrer", referrerUrl);
+                            custom.put("referrer_click_timestamp_seconds", response.getReferrerClickTimestampSeconds());
+                            custom.put("install_begin_timestamp_seconds", response.getInstallBeginTimestampSeconds());
+                            custom.put("google_play_instant", response.getGooglePlayInstantParam());
+                            custom.put("install_version", response.getInstallVersion());
+
+                            JSONObject jsonObject = new JSONObject(custom);
+                            Countly.userData.setProperty("custom_referrer", jsonObject.toString(1));
+                            Countly.userData.save();
+
+                            referrerClient.endConnection();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
+                        // API not available on the current Play Store app.
+                        break;
+                    case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
+                        // Connection couldn't be established.
+                        break;
+                }
+            }
+
+            @Override
+            public void onInstallReferrerServiceDisconnected() {
+            }
+        });
     }
 
     public void onClickButtonCustomEvents(View v) {
