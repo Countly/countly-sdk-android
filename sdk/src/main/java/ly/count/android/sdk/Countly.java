6f9d83ddf34ff867e334de4ca4ad5efbc299546d
ArtursK <kadikis.arturs@gmail.com>
2021-09-17 11:06:11 +0300

    Updating SDK version for next release

--- sdk/src/main/java/ly/count/android/sdk/Countly.java
+++ sdk/src/main/java/ly/count/android/sdk/Countly.java
@@ -49,7 +49,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Countly {
 
-    private String DEFAULT_COUNTLY_SDK_VERSION_STRING = "20.11.10";
+    private String DEFAULT_COUNTLY_SDK_VERSION_STRING = "20.11.11";
     /**
      * Used as request meta data on every request
      */
