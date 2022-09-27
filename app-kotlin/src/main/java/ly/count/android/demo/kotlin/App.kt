55c57e2a3962e44433b87373ef06f7cb72461f80
ArtursK <kadikis.arturs@gmail.com>
2022-09-27 17:53:31 +0300

    Removing debug code from the kotlin example app

--- app-kotlin/src/main/java/ly/count/android/demo/kotlin/App.kt
+++ app-kotlin/src/main/java/ly/count/android/demo/kotlin/App.kt
@@ -3,22 +3,14 @@ package ly.count.android.demo.kotlin
 import android.app.Application
 import ly.count.android.sdk.Countly
 import ly.count.android.sdk.CountlyConfig
-import ly.count.android.sdk.CountlyStore
-import ly.count.android.sdk.ModuleLog
 
 //import ly.count.android.sdk.DeviceIdType
 
 class App :Application() {
-  val COUNTLY_SERVER_URL = "https://master.count.ly"
-  val COUNTLY_APP_KEY = "5e20d03806255d314eb6679b26fda6e580b3d899"
+  val COUNTLY_SERVER_URL = "YOUR_SERVER"
+  val COUNTLY_APP_KEY = "YOUR_APP_KEY"
   override fun onCreate() {
     super.onCreate()
-
-    val cs = CountlyStore(this, ModuleLog())
-    cs.deviceID = "asd"
-
-    //var config : CountlyConfig = (CountlyConfig(this, COUNTLY_APP_KEY, COUNTLY_SERVER_URL)).setLoggingEnabled(true);
-    //Countly.sharedInstance().init(config)
 
     val countlyConfig = CountlyConfig(
       this,
