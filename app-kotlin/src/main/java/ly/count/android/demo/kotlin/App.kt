3794fc730e3ffb3dc7bae818809d0b715e1f08c3
turtledreams <deniztaygunerten@gmail.com>
2022-12-06 16:10:43 +0300

    urls

--- app-kotlin/src/main/java/ly/count/android/demo/kotlin/App.kt
+++ app-kotlin/src/main/java/ly/count/android/demo/kotlin/App.kt
@@ -7,7 +7,7 @@ import ly.count.android.sdk.CountlyConfig
 //import ly.count.android.sdk.DeviceIdType
 
 class App : Application() {
-  val COUNTLY_SERVER_URL = "https://yea.yea"
+  val COUNTLY_SERVER_URL = "YOUR_SERVER_URL"
   val COUNTLY_APP_KEY = "YOUR_APP_KEY"
   override fun onCreate() {
     super.onCreate()
