9023b3d7db37e26ea3de85272af058cfcd2bc315
ArtursK <kadikis.arturs@gmail.com>
2023-03-18 19:59:37 +0200

    Increasing SDK version

--- sdk/src/androidTest/java/ly/count/android/sdk/ConnectionQueueTests.java
+++ sdk/src/androidTest/java/ly/count/android/sdk/ConnectionQueueTests.java
@@ -493,7 +493,7 @@ public class ConnectionQueueTests {
                         break;
                     case "sdk_version":
                         if (a == 0) {
-                            Assert.assertTrue(pair[1].equals("22.09.0"));
+                            Assert.assertTrue(pair[1].equals("22.09.1"));
                         } else if (a == 1) {
                             Assert.assertTrue(pair[1].equals("123sdf.v-213"));
                         }
