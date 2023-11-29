cc733da8fcff3909a81af25541eaf053874e9c76
ArtursK <kadikis.arturs@gmail.com>
2023-11-29 15:49:38 +0200

    deleting unused things in benchmark app

--- app-benchmark/src/main/java/ly/count/android/benchmark/App.java
+++ app-benchmark/src/main/java/ly/count/android/benchmark/App.java
@@ -7,7 +7,7 @@ import ly.count.android.sdk.CountlyStore;
 import ly.count.android.sdk.ModuleLog;
 
 public class App extends Application {
-    final String COUNTLY_SERVER_URL = "https://try.count.ly";
+    final String COUNTLY_SERVER_URL = "https://xxx.count.ly";
     final String COUNTLY_APP_KEY = "YOUR_APP_KEY";
     final String DEVICE_ID = "YOUR_DEVICE_ID";
 
--- app-benchmark/src/main/java/ly/count/android/benchmark/MainActivity.java
+++ app-benchmark/src/main/java/ly/count/android/benchmark/MainActivity.java
@@ -18,9 +18,6 @@ import java.util.concurrent.TimeUnit;
 import ly.count.android.sdk.Countly;
 
 public class MainActivity extends AppCompatActivity {
-
-    InstallReferrerClient referrerClient;
-
     Benchmark benchmark;
 
     int loop = 10;
@@ -32,62 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         benchmark = new Benchmark();
-
-        referrerClient = InstallReferrerClient.newBuilder(this).build();
-        referrerClient.startConnection(new InstallReferrerStateListener() {
-            @Override
-            public void onInstallReferrerSetupFinished(int responseCode) {
-                switch (responseCode) {
-                    case InstallReferrerClient.InstallReferrerResponse.OK:
-                        try {
-                            ReferrerDetails response = referrerClient.getInstallReferrer();
-
-                            //you would retrieve the referrer url
-                            String referrerUrl = response.getInstallReferrer();
-
-                            //and then you would parse it and retrieve the required field to identify the campaign id and user id
-                            String campaignId = "someCampaignId";
-                            String userId = "someUserId";
-
-                            // The string is usually URL Encoded, so we need to decode it.
-                            String referrer = URLDecoder.decode(referrerUrl, "UTF-8");
-
-                            // Log the referrer string.
-                            Log.d(Countly.TAG, "Received Referrer url: " + referrer);
-
-                            //retrieve specific parts from the referrer url
-                            String[] parts = referrer.split("&");
-                            for (String part : parts) {
-                                if (part.startsWith("countly_cid")) {
-                                    campaignId = part.replace("countly_cid=", "").trim();
-                                }
-                                if (part.startsWith("countly_cuid")) {
-                                    userId = part.replace("countly_cuid=", "").trim();
-                                }
-                            }
-
-                            //you would then pass those retrieved values as manual attribution:
-                            Countly.sharedInstance().attribution().recordDirectAttribution("countly", "{\"cid\":" + campaignId + ",\"cuid\":" + userId + "}");
-                            //Countly.sharedInstance().attribution().recordDirectAttribution(campaignId, userId);
-
-                            referrerClient.endConnection();
-                        } catch (Exception e) {
-                            Countly.sharedInstance().L.e("[MainActivity] onInstallReferrerSetupFinished, Failed to get install referrer", e);
-                        }
-                        break;
-                    case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
-                        // API not available on the current Play Store app.
-                        break;
-                    case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
-                        // Connection couldn't be established.
-                        break;
-                }
-            }
-
-            @Override
-            public void onInstallReferrerServiceDisconnected() {
-            }
-        });
     }
 
     public void onHardKillPressed(View v) {
