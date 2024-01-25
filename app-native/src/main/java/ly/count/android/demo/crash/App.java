9113d29bd528b4c7c01620786bf2126d349b33cf
Artūrs Kadiķis <kadikis.arturs@gmail.com>
2024-01-25 16:33:38 +0200

    Update App.java

--- app-native/src/main/java/ly/count/android/demo/crash/App.java
+++ app-native/src/main/java/ly/count/android/demo/crash/App.java
@@ -8,11 +8,12 @@ import ly.count.android.sdknative.CountlyNative;
 
 public class App extends Application {
 
-    final static String COUNTLY_SERVER_URL = "https://your.server.ly";
-    final static String COUNTLY_APP_KEY = "YOUR_APP_KEY";
+    final String COUNTLY_SERVER_URL = "https://your.server.ly";
+    final String COUNTLY_APP_KEY = "YOUR_APP_KEY";
 
     @Override public void onCreate() {
         super.onCreate();
+        
         if (COUNTLY_SERVER_URL.equals("https://your.server.ly") || COUNTLY_APP_KEY.equals("YOUR_APP_KEY")) {
             Log.e("CountlyCrashDemo", "Please provide correct COUNTLY_SERVER_URL and COUNTLY_APP_KEY");
             return;
