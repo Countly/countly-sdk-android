941f6ba53f99cad031d47b8bf5e6f1068b53c8aa
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-03-08 16:10:51 +0300

    feat: add comments

--- sdk/src/main/java/ly/count/android/sdk/CrashData.java
+++ sdk/src/main/java/ly/count/android/sdk/CrashData.java
@@ -2,6 +2,9 @@ package ly.count.android.sdk;
 
 import java.util.Map;
 
+/**
+ * This class is used to store crash data.
+ */
 public class CrashData {
 
     private String stackTrace;
@@ -15,33 +18,55 @@ public class CrashData {
         this.breadcrumbs = breadcrumbs;
     }
 
+    /**
+     * Get the stack trace of the crash.
+     * @return the stack trace of the crash.
+     */
     public String getStackTrace() {
         return stackTrace;
     }
 
+    /**
+     * Get the segmentation of the crash.
+     * @return the segmentation of the crash.
+     */
     public Map<String,Object> getCrashSegmentation() {
         return crashSegmentation;
     }
 
+    /**
+     * Get the breadcrumbs of the crash.
+     * @return the breadcrumbs of the crash.
+     */
     public String getBreadcrumbs() {
         return breadcrumbs;
     }
 
+    /**
+     * Set the stack trace of the crash.
+     * @param stackTrace the stack trace of the crash.
+     */
     public void setStackTrace(String stackTrace) {
         this.stackTrace = stackTrace;
         crashChanged = true;
     }
 
+    /**
+     * Set the segmentation of the crash.
+     * @param crashSegmentation the segmentation of the crash.
+     */
     public void setCrashSegmentation(Map<String,Object> crashSegmentation) {
         this.crashSegmentation = crashSegmentation;
         crashChanged = true;
 
     }
 
-
+    /**
+     * Set the breadcrumbs of the crash.
+     * @param breadcrumbs the breadcrumbs of the crash.
+     */
     public void setBreadcrumbs(String breadcrumbs) {
         this.breadcrumbs = breadcrumbs;
         crashChanged = true;
-
     }
 }
