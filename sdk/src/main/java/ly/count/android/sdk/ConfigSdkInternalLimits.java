af475c3f40aa385e84e490df188c05068128f379
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-03-13 15:50:58 +0300

    feat: further comments

--- sdk/src/main/java/ly/count/android/sdk/ConfigSdkInternalLimits.java
+++ sdk/src/main/java/ly/count/android/sdk/ConfigSdkInternalLimits.java
@@ -12,7 +12,7 @@ public class ConfigSdkInternalLimits {
 
     /**
      * Sets how many segmentation values can be recorded when recording an event or view.
-     * Values exceeding this count will be ignored.
+     * Values exceeding this count will be ignored. The default value is 100 developer entries.
      *
      * @param maxSegmentationValues to set
      * @return Returns the same config object for convenient linking
@@ -25,6 +25,7 @@ public class ConfigSdkInternalLimits {
     /**
      * Set the maximum amount of breadcrumbs that can be recorded.
      * After exceeding the limit, the oldest values will be removed.
+     * Default value is 100.
      *
      * @param maxBreadcrumbCount to set
      * @return Returns the same config object for convenient linking
@@ -34,14 +35,30 @@ public class ConfigSdkInternalLimits {
         return this;
     }
 
+    /**
+     * Set the maximum key length for keys used internally. This affects things like: event names, view names,
+     * custom trace key name (APM), custom metric key (APM), segmentation keys, custom user property keys, custom user property keys
+     * that are used for property modifiers (mul, push, pull, set, increment, etc)
+     * Default value is 128.
+     *
+     * @param maxKeyLength to set
+     * @return Returns the same config object for convenient linking
+     */
     public synchronized ConfigSdkInternalLimits setMaxKeyLength(int maxKeyLength) {
         this.maxKeyLength = maxKeyLength;
         return this;
     }
 
     /**
-     * Set the maximum value size for values used internally. This affects things like: segmentation values
-     * user property values, breadcrumb text.
+     * Set the maximum value size for values used internally.
+     * - segmentation value in case of strings (for all features)
+     * - custom user property string value
+     * - user profile named key (username, email, etc) string values. Except "picture" field, that has a limit of 4096 chars
+     * - custom user property modifier string values. For example, for modifiers like "push", "pull", "setOnce", etc.
+     * - breadcrumb text
+     * - manual feedback widget reporting fields (reported as event)
+     * - rating widget response (reported as event)
+     * Default value is 256.
      * If those values exceed the set limit, they will be truncated.
      *
      * @param maxValueSize to set
@@ -54,6 +71,7 @@ public class ConfigSdkInternalLimits {
 
     /**
      * Set the maximum amount of stack trace lines that can be recorded per thread.
+     * Default value is 30.
      *
      * @param maxStackTraceLinesPerThread to set
      * @return Returns the same config object for convenient linking
@@ -65,6 +83,7 @@ public class ConfigSdkInternalLimits {
 
     /**
      * Set the maximum amount of stack trace threads that can be recorded.
+     * Default value is 200.
      *
      * @param maxStackTraceLineLength to set
      * @return Returns the same config object for convenient linking
