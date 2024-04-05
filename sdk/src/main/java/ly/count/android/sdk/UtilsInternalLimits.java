078ecef45af914e01dbef1476d7317568dc64877
ArtursK <kadikis.arturs@gmail.com>
2024-04-05 14:46:53 +0300

    Change null possibility

--- sdk/src/main/java/ly/count/android/sdk/UtilsInternalLimits.java
+++ sdk/src/main/java/ly/count/android/sdk/UtilsInternalLimits.java
@@ -180,15 +180,10 @@ public class UtilsInternalLimits {
      * @param L ModuleLog @NonNull - logger
      * @param tag String @NonNull - tag to use in logs
      */
-    protected static void applySdkInternalLimitsToSegmentation(@Nullable Map<String, Object> segmentation, @NonNull ConfigSdkInternalLimits limitsConfig, @NonNull ModuleLog L, @NonNull String tag) {
+    protected static void applySdkInternalLimitsToSegmentation(@NonNull Map<String, Object> segmentation, @NonNull ConfigSdkInternalLimits limitsConfig, @NonNull ModuleLog L, @NonNull String tag) {
         assert limitsConfig != null;
         assert L != null;
         assert tag != null;
-
-        if (segmentation == null) {
-            L.w(tag + ": [UtilsSdkInternalLimits] applySdkInternalLimitsToSegmentation, map is null, returning");
-            return;
-        }
 
         if (segmentation.isEmpty()) {
             L.w(tag + ": [UtilsSdkInternalLimits] applySdkInternalLimitsToSegmentation, map is empty, returning");
@@ -251,11 +246,7 @@ public class UtilsInternalLimits {
      * @param messagePrefix String @NonNull - name of the module this function was called
      * @param segmentation Map<String, Object> @Nullable- segmentation that will be checked
      */
-    static void truncateSegmentationValues(@Nullable final Map<String, Object> segmentation, final int maxCount, @NonNull final String messagePrefix, final @NonNull ModuleLog L) {
-        if (segmentation == null) {
-            return;
-        }
-
+    static void truncateSegmentationValues(@NonNull final Map<String, Object> segmentation, final int maxCount, @NonNull final String messagePrefix, final @NonNull ModuleLog L) {
         assert segmentation != null;
         assert maxCount >= 1;
         assert L != null;
@@ -282,11 +273,7 @@ public class UtilsInternalLimits {
      * @param messagePrefix
      * @param L
      */
-    static void removeReservedKeysFromSegmentation(@Nullable Map<String, Object> segmentation, @NonNull String[] reservedKeys, @NonNull String messagePrefix, @NonNull ModuleLog L) {
-        if (segmentation == null) {
-            return;
-        }
-
+    static void removeReservedKeysFromSegmentation(@NonNull Map<String, Object> segmentation, @NonNull String[] reservedKeys, @NonNull String messagePrefix, @NonNull ModuleLog L) {
         assert segmentation != null;
         assert reservedKeys != null;
         assert L != null;
@@ -306,11 +293,7 @@ public class UtilsInternalLimits {
      * @param data
      * @return returns true if any entry had been removed
      */
-    static boolean removeUnsupportedDataTypes(Map<String, Object> data) {
-        if (data == null) {
-            return false;
-        }
-
+    static boolean removeUnsupportedDataTypes(@NonNull Map<String, Object> data) {
         assert data != null;
 
         boolean removed = false;
@@ -334,7 +317,7 @@ public class UtilsInternalLimits {
         return removed;
     }
 
-    static boolean isSupportedDataType(Object value) {
+    static boolean isSupportedDataType(@Nullable Object value) {
         return value instanceof String || value instanceof Integer || value instanceof Double || value instanceof Boolean || value instanceof Float;
     }
 }
