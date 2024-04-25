8c53cf11a31f5cb505cd9072480bd774f5f5f0c6
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-04-25 16:59:15 +0300

    fix: codacy changes for native app

--- app-native/src/main/java/ly/count/android/demo/crash/App.java
+++ app-native/src/main/java/ly/count/android/demo/crash/App.java
@@ -8,13 +8,13 @@ import ly.count.android.sdknative.CountlyNative;
 
 public class App extends Application {
 
-    final String COUNTLY_SERVER_URL = "https://your.server.ly";
-    final String COUNTLY_APP_KEY = "YOUR_APP_KEY";
+    private final static String COUNTLY_SERVER_URL = "https://your.server.ly";
+    private final static String COUNTLY_APP_KEY = "YOUR_APP_KEY";
 
     @Override public void onCreate() {
         super.onCreate();
-        
-        if (COUNTLY_SERVER_URL.equals("https://your.server.ly") || COUNTLY_APP_KEY.equals("YOUR_APP_KEY")) {
+
+        if ("https://your.server.ly".equals(COUNTLY_SERVER_URL) || "YOUR_APP_KEY".equals(COUNTLY_APP_KEY)) {
             Log.e("CountlyCrashDemo", "Please provide correct COUNTLY_SERVER_URL and COUNTLY_APP_KEY");
             return;
         }
