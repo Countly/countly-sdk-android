56bc4c9ed5645704de58e9031222873155afe80f
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-04-25 17:07:06 +0300

    fix: codacy changes for app

--- app/src/main/java/ly/count/android/demo/ActivityExampleTests.java
+++ app/src/main/java/ly/count/android/demo/ActivityExampleTests.java
@@ -1,15 +1,14 @@
 package ly.count.android.demo;
 
+import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import java.util.Arrays;
 import java.util.Map;
 import ly.count.android.sdk.Countly;
 import ly.count.android.sdk.ExperimentInformation;
-import ly.count.android.sdk.RCVariantCallback;
 import ly.count.android.sdk.RequestResult;
 
 public class ActivityExampleTests extends AppCompatActivity {
@@ -23,37 +22,33 @@ public class ActivityExampleTests extends AppCompatActivity {
     // gets and prints all stored experiment information
     public void onClickExperimentsPrintValues(View v) {
         Map<String, ExperimentInformation> values = Countly.sharedInstance().remoteConfig().testingGetAllExperimentInfo();
-        if (values == null) {
-            Countly.sharedInstance().L.w("No experiments present");
-            return;
-        }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(28);
         sb.append("Experiment Information:\n");
 
         for (Map.Entry<String, ExperimentInformation> entry : values.entrySet()) {
             String expID = entry.getKey();
             ExperimentInformation experimentInfo = entry.getValue();
 
-            sb.append("Experiment ID: ").append(expID).append("\n");
-            sb.append("Experiment Name: ").append(experimentInfo.experimentName).append("\n");
-            sb.append("Experiment Description: ").append(experimentInfo.experimentDescription).append("\n");
-            sb.append("Current Variant: ").append(experimentInfo.currentVariant).append("\n");
+            sb.append("Experiment ID: ").append(expID);
+            sb.append("\nExperiment Name: ").append(experimentInfo.experimentName);
+            sb.append("\nExperiment Description: ").append(experimentInfo.experimentDescription);
+            sb.append("\nCurrent Variant: ").append(experimentInfo.currentVariant);
 
             for (Map.Entry<String, Map<String, Object>> variantEntry : experimentInfo.variants.entrySet()) {
                 String variantName = variantEntry.getKey();
                 Map<String, Object> variantAttributes = variantEntry.getValue();
 
-                sb.append("Variant: ").append(variantName).append("\n");
+                sb.append("\nVariant: ").append(variantName).append('\n');
                 for (Map.Entry<String, Object> attributeEntry : variantAttributes.entrySet()) {
                     String attributeName = attributeEntry.getKey();
                     Object attributeValue = attributeEntry.getValue();
 
-                    sb.append(attributeName).append(": ").append(attributeValue).append("\n");
+                    sb.append(attributeName).append(": ").append(attributeValue).append('\n');
                 }
             }
 
-            sb.append("\n");
+            sb.append('\n');
         }
 
         Toast t = Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_LONG);
@@ -63,28 +58,22 @@ public class ActivityExampleTests extends AppCompatActivity {
 
     // For fetching all variants with a button click
     public void onClickFetchAllExperiments(View v) {
-        Countly.sharedInstance().remoteConfig().testingDownloadExperimentInformation(new RCVariantCallback() {
-            @Override
-            public void callback(RequestResult result, String error) {
-                if (result == RequestResult.Success) {
-                    Toast.makeText(getApplicationContext(), "Fetch finished", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Error: " + result, Toast.LENGTH_SHORT).show();
-                }
+        Countly.sharedInstance().remoteConfig().testingDownloadExperimentInformation((result, error) -> {
+            if (result == RequestResult.Success) {
+                Toast.makeText(getApplicationContext(), "Fetch finished", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Error: " + result, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     // For fetching all variants with a button click
     public void onClickFetchAllVariants(View v) {
-        Countly.sharedInstance().remoteConfig().testingDownloadVariantInformation(new RCVariantCallback() {
-            @Override
-            public void callback(RequestResult result, String error) {
-                if (result == RequestResult.Success) {
-                    Toast.makeText(getApplicationContext(), "Fetch finished", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Error: " + result, Toast.LENGTH_SHORT).show();
-                }
+        Countly.sharedInstance().remoteConfig().testingDownloadVariantInformation((result, error) -> {
+            if (result == RequestResult.Success) {
+                Toast.makeText(getApplicationContext(), "Fetch finished", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Error: " + result, Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -92,10 +81,6 @@ public class ActivityExampleTests extends AppCompatActivity {
     // To get all variants from the storage and show them with a toast
     public void onClickVariantsPrintValues(View v) {
         Map<String, String[]> values = Countly.sharedInstance().remoteConfig().testingGetAllVariants();
-        if (values == null) {
-            Countly.sharedInstance().L.w("No variants present");
-            return;
-        }
         Countly.sharedInstance().L.d("Get all variants: " + values);
 
         StringBuilder sb = new StringBuilder();
@@ -113,29 +98,18 @@ public class ActivityExampleTests extends AppCompatActivity {
 
     public void onClickEnrollVariant(View v) {
         Map<String, String[]> values = Countly.sharedInstance().remoteConfig().testingGetAllVariants();
-        if (values == null) {
-            Countly.sharedInstance().L.w("No variants present");
-            return;
-        }
-        Countly.sharedInstance().L.d("Get all variants: [" + values.toString() + "]");
+        Countly.sharedInstance().L.d("Get all variants: [" + values + "]");
 
+        Map.Entry<String, String[]> entry = values.entrySet().iterator().next();
         // Get the first key and variant
-        String key = null;
-        String variant = null;
-        for (Map.Entry<String, String[]> entry : values.entrySet()) {
-            key = entry.getKey();
-            variant = entry.getValue()[0]; // first variant
-            break; // Get only the first key-value pair
-        }
+        String key = entry.getKey();
+        String variant = entry.getValue()[0];
 
-        Countly.sharedInstance().remoteConfig().testingEnrollIntoVariant(key, variant, new RCVariantCallback() {
-            @Override
-            public void callback(RequestResult result, String error) {
-                if (result == RequestResult.Success) {
-                    Toast.makeText(getApplicationContext(), "Fetch finished", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Error: " + result, Toast.LENGTH_SHORT).show();
-                }
+        Countly.sharedInstance().remoteConfig().testingEnrollIntoVariant(key, variant, (result, error) -> {
+            if (result == RequestResult.Success) {
+                Toast.makeText(getApplicationContext(), "Fetch finished", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Error: " + result, Toast.LENGTH_SHORT).show();
             }
         });
     }
