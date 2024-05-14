23e05f07d41ee639b485a94502d475184cad768a
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-05-14 16:42:24 +0300

    feat: test scenario for session duration

--- sdk/src/androidTest/java/ly/count/android/sdk/DeviceIdTests.java
+++ sdk/src/androidTest/java/ly/count/android/sdk/DeviceIdTests.java
@@ -1,7 +1,10 @@
 package ly.count.android.sdk;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -248,5 +251,114 @@ public class DeviceIdTests {
         DeviceId did = new DeviceId("123", store, mock(ModuleLog.class), openUDIDProvider);
         assertEquals("xxx", did.getCurrentId());
         assertEquals(DeviceIdType.OPEN_UDID, did.getType());
+    }
+
+    /**
+     * Case where multiple device ID change calls affect the session duration
+     * This is a scenario where the session duration is correctly calculated and sent
+     * ---
+     * The session duration is calculated based on the time between the last two device ID changes
+     * And for every device ID change, a session duration is sent flow should be like this:
+     * ---
+     * set a couple of user properties
+     * sleep 1 second
+     * change device ID with merge (creates a session duration request with the duration of 1 second)
+     * save user profiles
+     * sleep 2 seconds
+     * change device ID without merge (creates a session duration request with the duration of 2 seconds)
+     * -- at this point 4 requests are generated (firs begin, 1 merge, 1 user profile, 1 end session for without merge)
+     * sleep 1 second
+     * set user property and save it
+     * change device ID without merge with same device id (no session duration request)
+     * set user properties
+     * sleep 1 second
+     * change device ID without merge with same device id (no session duration request)
+     * save user properties
+     * change device ID without merge with same device id (no session duration request)
+     * sleep 1 second
+     * set user properties and save them
+     * sleep 1 second
+     * change device ID with merge (creates a session duration request with the duration of 4 seconds)
+     * -- at this point 8 requests are generated (firs begin, 1 merge, 1 user profile, 1 end session for without merge, 1 user profile, 1 user profile, 1 user profile, 1 merge)
+     */
+    @Test
+    public void sessionDurationScenario_1() throws InterruptedException {
+        CountlyConfig config = TestUtils.createBaseConfig();
+        config.setRequiresConsent(false);
+        config.lifecycleObserver = () -> true;
+
+        Countly countly = new Countly().init(config);
+        validateSessionRequest(0, null, null, false);
+
+        countly.userProfile().setProperty("prop1", "string");
+        countly.userProfile().setProperty("prop2", 123);
+        countly.userProfile().setProperty("prop3", false);
+
+        Thread.sleep(1000);
+
+        countly.deviceId().changeWithMerge("ff_merge"); // this will generate a request with "session_duration" field and reset duration
+        countly.userProfile().save();
+
+        Thread.sleep(2000);
+
+        countly.deviceId().changeWithoutMerge("ff"); // this will generate a request with "end_session", "session_duration" fields and reset duration
+        Assert.assertEquals(4, TestUtils.getCurrentRQ().length);
+        validateSessionRequest(1, 1, "ff_merge", false);
+        validateSessionRequest(3, 2, null, true);
+
+        Thread.sleep(1000);
+
+        countly.userProfile().setProperty("prop4", new String[] { "sd" });
+        countly.userProfile().save();
+        countly.deviceId().changeWithoutMerge("ff"); // this will not affect the session duration
+        countly.userProfile().setProperty("prop5", new HashMap<String, Object>() {{
+            put("key", "value");
+        }});
+        countly.userProfile().setProperty("prop6", new HashMap<String, Object>() {{
+            put("key", 123);
+        }});
+        countly.userProfile().setProperty("prop7", new HashMap<String, Object>() {{
+            put("key", false);
+        }});
+
+        Thread.sleep(1000);
+
+        countly.deviceId().changeWithoutMerge("ff"); // this will not affect the session duration
+        countly.userProfile().save();
+        countly.deviceId().changeWithoutMerge("ff"); // this will not affect the session duration
+
+        Thread.sleep(1000);
+
+        countly.userProfile().setProperty("prop1", "string_a");
+        countly.userProfile().setProperty("prop2", 456);
+        countly.userProfile().setProperty("prop3", true);
+        countly.userProfile().save();
+
+        Thread.sleep(1000);
+
+        countly.deviceId().changeWithMerge("ff_merge"); // this will generate a request with "session_duration" field and reset duration
+
+        Assert.assertEquals(8, TestUtils.getCurrentRQ().length);
+        validateSessionRequest(7, 4, "ff_merge", false);
+    }
+
+    void validateSessionRequest(int idx, Integer duration, String deviceId, boolean endSession) {
+        Map<String, String> request = TestUtils.getCurrentRQ()[idx];
+
+        if (deviceId != null) {
+            TestUtils.validateRequiredParams(request, deviceId);
+        } else {
+            TestUtils.validateRequiredParams(request);
+        }
+
+        if (endSession) {
+            assertTrue(request.containsKey("end_session"));
+        }
+
+        if (duration != null) {
+            assertEquals(duration, Integer.valueOf(request.get("session_duration")));
+        } else {
+            assertTrue(request.containsKey("begin_session"));
+        }
     }
 }
