4978117649b8c44c517f9f6a077e9e2805bc94f1
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-06-28 17:46:56 +0300

    fix: make others protected

--- sdk/src/main/java/ly/count/android/sdk/ActivityConstants.java
+++ sdk/src/main/java/ly/count/android/sdk/ActivityConstants.java
@@ -13,20 +13,20 @@ public class ActivityConstants {
     public static class Orientation {
         public static final int TOP_HALF = 1;
         public static final int BOTTOM_HALF = 2;
-        public static final int TOP_BAR = 3;
-        public static final int BOTTOM_BAR = 4;
-        public static final int CENTER_PADDED = 5;
-        public static final int CENTER_WHOLE = 6;
-        public static final int BOTTOM_RIGHT_QUARTER = 7;
-        public static final int BOTTOM_LEFT_QUARTER = 8;
-        public static final int TOP_RIGHT_QUARTER = 9;
-        public static final int TOP_LEFT_QUARTER = 10;
+        protected static final int TOP_BAR = 3;
+        protected static final int BOTTOM_BAR = 4;
+        protected static final int CENTER_PADDED = 5;
+        protected static final int CENTER_WHOLE = 6;
+        protected static final int BOTTOM_RIGHT_QUARTER = 7;
+        protected static final int BOTTOM_LEFT_QUARTER = 8;
+        protected static final int TOP_RIGHT_QUARTER = 9;
+        protected static final int TOP_LEFT_QUARTER = 10;
     }
 
     public static class Animate {
-        public static final int TOP_TO_CENTER = 1;
-        public static final int BOTTOM_BAR_TO_PADDED_BOTTOM_HALF = 2;
-        public static final int CENTER_PADDED_TO_BOTTOM_HALF = 3;
-        public static final int BOTTOM_BAR_TO_WHOLE_SCREEN = 4;
+        protected static final int TOP_TO_CENTER = 1;
+        protected static final int BOTTOM_BAR_TO_PADDED_BOTTOM_HALF = 2;
+        protected static final int CENTER_PADDED_TO_BOTTOM_HALF = 3;
+        protected static final int BOTTOM_BAR_TO_WHOLE_SCREEN = 4;
     }
 }
