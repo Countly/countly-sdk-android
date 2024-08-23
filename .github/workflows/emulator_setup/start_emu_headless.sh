89f63f37cfed8bd1d3536759400670ac73dc206b
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-08-23 15:32:08 +0300

    feat: accel on

--- .github/workflows/emulator_setup/start_emu_headless.sh
+++ .github/workflows/emulator_setup/start_emu_headless.sh
@@ -8,34 +8,9 @@ NC='\033[0m' # No Color
 
 emulator_name=${EMULATOR_NAME}
 
-function check_hardware_acceleration() {
-    if [[ "$HW_ACCEL_OVERRIDE" != "" ]]; then
-        hw_accel_flag="$HW_ACCEL_OVERRIDE"
-    else
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-            # macOS-specific hardware acceleration check
-            HW_ACCEL_SUPPORT=$(sysctl -a | grep -E -c '(vmx|svm)')
-        else
-            # generic Linux hardware acceleration check
-            HW_ACCEL_SUPPORT=$(grep -E -c '(vmx|svm)' /proc/cpuinfo)
-        fi
-
-        if [[ $HW_ACCEL_SUPPORT == 0 ]]; then
-            hw_accel_flag="-accel off"
-        else
-            hw_accel_flag="-accel on"
-        fi
-    fi
-
-    echo "$hw_accel_flag"
-}
-
-
-hw_accel_flag=$(check_hardware_acceleration)
-
 function launch_emulator () {
   adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
-  options="@${emulator_name} -no-window -no-snapshot -noaudio -no-boot-anim -memory 2048 ${hw_accel_flag} -camera-back none"
+  options="@${emulator_name} -no-window -no-snapshot -noaudio -no-boot-anim -memory 2048 -accel on -camera-back none"
   if [[ "$OSTYPE" == *linux* ]]; then
     echo "${OSTYPE}: emulator ${options} -gpu off"
     nohup emulator $options -gpu off &
