a9a6ab11a60c565a3199c477a9c43c7b0bb2a640
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-09-05 14:24:56 +0300

    feat: experimental notes to config

--- sdk/src/main/java/ly/count/android/sdk/ConfigExperimental.java
+++ sdk/src/main/java/ly/count/android/sdk/ConfigExperimental.java
@@ -4,11 +4,25 @@ public class ConfigExperimental {
     protected boolean viewNameRecordingEnabled = false;
     protected boolean visibilityTrackingEnabled = false;
 
+    /**
+     * This will enabled view name recording for previous view for event and views
+     * and current view name for events.
+     *
+     * @return ConfigExperimental instance for chaining
+     * @apiNote This is an EXPERIMENTAL feature, and it can have breaking changes
+     */
     public ConfigExperimental enableViewNameRecording() {
         viewNameRecordingEnabled = true;
         return this;
     }
 
+    /**
+     * This will enable visibility tracking for events and views.
+     * This will track at the view or event creation time, if the app is in the foreground or background.
+     *
+     * @return ConfigExperimental instance for chaining
+     * @apiNote This is an EXPERIMENTAL feature, and it can have breaking changes
+     */
     public ConfigExperimental enableVisibilityTracking() {
         visibilityTrackingEnabled = true;
         return this;
