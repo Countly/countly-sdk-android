c847fda4972adbe04424fee50feaf35c994e1980
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-12-02 15:06:22 +0300

    fix: RC

--- sdk/src/main/java/ly/count/android/sdk/Countly.java
+++ sdk/src/main/java/ly/count/android/sdk/Countly.java
@@ -47,7 +47,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Countly {
 
-    private final String DEFAULT_COUNTLY_SDK_VERSION_STRING = "24.7.7";
+    private final String DEFAULT_COUNTLY_SDK_VERSION_STRING = "24.7.7-RC1";
 
     /**
      * Used as request meta data on every request
