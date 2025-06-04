8e89a5d4889498d9f5809b0e155f269c62697bbd
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-06-04 10:45:21 +0300

    feat: 25.4.1

--- sdk/src/main/java/ly/count/android/sdk/Countly.java
+++ sdk/src/main/java/ly/count/android/sdk/Countly.java
@@ -47,7 +47,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Countly {
 
-    private final String DEFAULT_COUNTLY_SDK_VERSION_STRING = "25.4.1-RC3";
+    private final String DEFAULT_COUNTLY_SDK_VERSION_STRING = "25.4.1";
     /**
      * Used as request meta data on every request
      */
