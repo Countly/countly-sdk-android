004c4b9bdb5fcfd7f96fea861d46b9a08a43278e
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-08-27 11:15:16 +0300

    fix: add locale to the cases

--- sdk/src/main/java/ly/count/android/sdk/PreflightRequestMaker.java
+++ sdk/src/main/java/ly/count/android/sdk/PreflightRequestMaker.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.Locale;
 import org.json.JSONObject;
 
 class PreflightRequestMaker extends AsyncTask<Object, Void, Boolean> implements ImmediateRequestI {
@@ -55,7 +56,7 @@ class PreflightRequestMaker extends AsyncTask<Object, Void, Boolean> implements 
             String contentType = connection.getHeaderField("Content-Type");
 
             return (responseCode == HttpURLConnection.HTTP_OK) &&
-                (contentType != null && contentType.toLowerCase().contains("text/html"));
+                (contentType != null && contentType.toLowerCase(Locale.US).contains("text/html"));
         } catch (Exception e) {
             L.e("[ImmediateRequestMaker] doPreflightRequest, Received exception while making a immediate server request", e);
         } finally {
