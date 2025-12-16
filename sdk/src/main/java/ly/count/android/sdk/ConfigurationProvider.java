59b62cddd73b482a2addcc21762a52117467079f
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-12-16 16:37:49 +0300

    feat: add methods to the config provider

--- sdk/src/main/java/ly/count/android/sdk/ConfigurationProvider.java
+++ sdk/src/main/java/ly/count/android/sdk/ConfigurationProvider.java
@@ -1,5 +1,8 @@
 package ly.count.android.sdk;
 
+import java.util.Map;
+import java.util.Set;
+
 interface ConfigurationProvider {
     boolean getNetworkingEnabled();
 
@@ -31,4 +34,17 @@ interface ConfigurationProvider {
     int getBOMDuration();
 
     int getRequestTimeoutDurationMillis();
+
+    int getUserPropertyCacheLimit();
+
+    // LISTING FILTERS
+    boolean getFilterIsWhitelist();
+
+    Set<String> getEventFilterSet();
+
+    Set<String> getUserPropertyFilterSet();
+
+    Set<String> getSegmentationFilterSet();
+
+    Map<String, Set<String>> getEventSegmentationFilterMap();
 }
