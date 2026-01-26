9e97fbc5f29a4d41ac69a5de6e2090f4bf22f3be
Arif Burak Demiray <burakdemiray09@hotmail.com>
2026-01-26 12:59:27 +0300

    feat: callback impl

--- sdk/src/main/java/ly/count/android/sdk/InternalRequestCallback.java
+++ sdk/src/main/java/ly/count/android/sdk/InternalRequestCallback.java
@@ -1,4 +1,9 @@
 package ly.count.android.sdk;
 
 interface InternalRequestCallback {
+    default void onRequestCompleted(String response, boolean success) {
+    }
+
+    default void onRQFinished() {
+    }
 }
