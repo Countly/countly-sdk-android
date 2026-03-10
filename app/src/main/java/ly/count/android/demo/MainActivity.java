48f786e76666c62165ca9097263cff1846e5fa0a
Arif Burak Demiray <burakdemiray09@hotmail.com>
2026-03-10 16:51:34 +0300

    refactor: modernize example app

--- app/src/main/java/ly/count/android/demo/MainActivity.java
+++ app/src/main/java/ly/count/android/demo/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import com.android.installreferrer.api.InstallReferrerClient;
 import com.android.installreferrer.api.InstallReferrerStateListener;
 import com.android.installreferrer.api.ReferrerDetails;
@@ -21,18 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        To send Referrer follow these steps.
-        Steps:
-        1. Start Google Play on the device using campaign link,
-        for example, https://play.google.com/store/apps/details?id=ly.count.android.demo&referrer=utm_source%3Dtest_source%26utm_medium%3Dtest_medium%26
-        utm_term%3Dtest-term%26utm_content%3Dtest_content%26utm_campaign%3Dtest_name
-        (You can use google play generator: https://developers.google.com/analytics/devguides/collection/android/v3/campaigns#google-play-url-builder)
-        2. DON'T TAP ON INSTALL BUTTON
-        3. Install your test build using adb.
+        TextView txtVersion = findViewById(R.id.txtSdkVersion);
+        txtVersion.setText("SDK v" + Countly.sharedInstance().COUNTLY_SDK_VERSION_STRING);
 
-        Google Play will be returning your test campaign now.
-         */
+        ImageView btnNightMode = findViewById(R.id.btnToggleNightMode);
+        btnNightMode.setOnClickListener(v -> {
+            int currentMode = AppCompatDelegate.getDefaultNightMode();
+            if (currentMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
+
         referrerClient = InstallReferrerClient.newBuilder(this).build();
         referrerClient.startConnection(new InstallReferrerStateListener() {
             @Override
@@ -41,21 +45,11 @@ public class MainActivity extends AppCompatActivity {
                     case InstallReferrerClient.InstallReferrerResponse.OK:
                         try {
                             ReferrerDetails response = referrerClient.getInstallReferrer();
-
-                            //you would retrieve the referrer url
                             String referrerUrl = response.getInstallReferrer();
-
-                            //and then you would parse it and retrieve the required field to identify the campaign id and user id
                             String campaignId = "someCampaignId";
                             String userId = "someUserId";
-
-                            // The string is usually URL Encoded, so we need to decode it.
                             String referrer = URLDecoder.decode(referrerUrl, "UTF-8");
-
-                            // Log the referrer string.
                             Log.d(Countly.TAG, "Received Referrer url: " + referrer);
-
-                            //retrieve specific parts from the referrer url
                             String[] parts = referrer.split("&");
                             for (String part : parts) {
                                 if (part.startsWith("countly_cid")) {
@@ -65,21 +59,15 @@ public class MainActivity extends AppCompatActivity {
                                     userId = part.replace("countly_cuid=", "").trim();
                                 }
                             }
-
-                            //you would then pass those retrieved values as manual attribution:
                             Countly.sharedInstance().attribution().recordDirectAttribution("countly", "{\"cid\":" + campaignId + ",\"cuid\":" + userId + "}");
-                            //Countly.sharedInstance().attribution().recordDirectAttribution(campaignId, userId);
-
                             referrerClient.endConnection();
                         } catch (Exception e) {
                             Log.e(demoTag, "Error while parsing referrer", e);
                         }
                         break;
                     case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
-                        // API not available on the current Play Store app.
                         break;
                     case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
-                        // Connection couldn't be established.
                         break;
                     default:
                         break;
@@ -90,8 +78,6 @@ public class MainActivity extends AppCompatActivity {
             public void onInstallReferrerServiceDisconnected() {
             }
         });
-
-        //ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.BROADCAST_CLOSE_SYSTEM_DIALOGS }, 123);
     }
 
     public void onClickButtonCustomEvents(View v) {
@@ -116,10 +102,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickButtonAutoViewTracking(View v) {
         startActivity(new Intent(this, ActivityExampleAutoViewTracking.class));
-    }
-
-    public void onClickButtonMultiThreading(View v) {
-        //
     }
 
     public void onClickButtonOthers(View v) {
@@ -148,5 +130,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickButtonFragments(View v) {
         startActivity(new Intent(this, ActivityExampleFragments.class));
+    }
+
+    public void onClickButtonConsent(View v) {
+        startActivity(new Intent(this, ActivityExampleConsent.class));
+    }
+
+    public void onClickButtonLocation(View v) {
+        startActivity(new Intent(this, ActivityExampleLocation.class));
+    }
+
+    public void onClickButtonSessions(View v) {
+        startActivity(new Intent(this, ActivityExampleSessions.class));
     }
 }
